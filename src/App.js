99fa3ce103c6e946ac912beada3ff88fcabcb691
rocionadal <rocioonadal@gmail.com>
2021-02-24 14:31:06 +0100

    import inp & out in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
 class App extends Component {
   state = {
     username: 'supermax'
