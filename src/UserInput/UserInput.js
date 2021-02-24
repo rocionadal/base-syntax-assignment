fb0b1e630e863dd0f9628eecf02aeb7f0feb7220
rocionadal <rocioonadal@gmail.com>
2021-02-24 15:07:19 +0100

    inline style for input

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const UserInput = (props) => {
     const inputStyle = {
-      border: '2px solid red'  
+      border: '2px solid #28527a',
+      padding: '4px',
+      marginLeft: '110px' 
     };
 
     return <input 
