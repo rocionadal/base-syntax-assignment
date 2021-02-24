8f0c8bf1caa8bb6a9168f53b075d4fca6e83eea8
rocionadal <rocioonadal@gmail.com>
2021-02-24 15:02:30 +0100

    stylesheet for outputs

--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './UserOutput.css';
+
 const UserOutput = (props) => {
     return (
-        <div>
+        <div className="UserOutput">
             <p>Username: {props.userName}</p>
             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </div>
