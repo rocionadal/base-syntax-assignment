19365c8aae1c29bcc2008c1b07112c2d63f2e8b7
rocionadal <rocioonadal@gmail.com>
2021-02-24 14:27:40 +0100

    add two p for comp

--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 const userOutput = () => {
-
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    )
 };
