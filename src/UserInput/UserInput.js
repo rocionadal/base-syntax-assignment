46a63071afc2ee3a083330b562043d8b88e08d66
rocionadal <rocioonadal@gmail.com>
2021-02-24 14:57:05 +0100

    inline style for input

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const UserInput = (props) => {
+    const inputStyle = {
+      border: '2px solid red'  
+    };
+
     return <input 
     type="text" 
+    style={inputStyle}
     onChange={props.changed}  
     value={props.currentName} />;
 };
